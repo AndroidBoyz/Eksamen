bca6e206af1ba7cca08efa146db14b652a3db2b2
Bjørnar Heggset Nes <bjornar.hn@hotmail.com>
2017-03-21 11:24:03 +0100

    Fb login på plass, acess token i orden. Neste blir å hente ut brukerdata med json.

---
+++ app/src/main/java/com/example/bjheggset/buckets/Login.java
@@ -0,0 +1,58 @@
+package com.example.bjheggset.buckets;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class Login extends AppCompatActivity {
+    TextView text;
+    LoginButton loginButton;
+    CallbackManager callbackManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_fblogin);
+
+
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        text = (TextView)findViewById(R.id.text);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                text.setText("Login success! \n" +
+                loginResult.getAccessToken().getUserId()+
+                "\n"+loginResult.getAccessToken().getToken());
+            }
+
+            @Override
+            public void onCancel() {
+
+                text.setText("Login cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+    }
+}
