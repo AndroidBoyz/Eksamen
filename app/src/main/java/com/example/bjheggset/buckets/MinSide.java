727178e048189cc3b0419b82976985865f9411a7
Bjørnar Heggset Nes <bjornar.hn@hotmail.com>
2017-03-29 08:25:34 +0200

    Henter profilbilde fra facebook til minside

--- app/src/main/java/com/example/bjheggset/buckets/MinSide.java
+++ app/src/main/java/com/example/bjheggset/buckets/MinSide.java
@@ -2,27 +2,35 @@ package com.example.bjheggset.buckets;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.facebook.AccessToken;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
-import com.facebook.HttpMethod;
 import com.facebook.Profile;
-import com.facebook.login.LoginResult;
+import com.facebook.login.widget.ProfilePictureView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MinSide extends AppCompatActivity {
 
     Intent i;
     Context $me = this;
+
 
 
     @Override
@@ -45,14 +53,20 @@ public class MinSide extends AppCompatActivity {
                     public void onCompleted(
                             JSONObject object,
                             GraphResponse response) {
+
+                        Profile profile = Profile.getCurrentProfile();
                         BackgroundWorker backgroundWorker = new BackgroundWorker($me);
                         TextView txtnavn = (TextView)findViewById(R.id.Navn);
+                        ((ProfilePictureView)findViewById(R.id.profilePicture)).setProfileId(
+                                profile.getId());
 
                      try {
                          String first_name = object.getString("first_name");
                          String last_name = object.getString("last_name");
                          String id = Profile.getCurrentProfile().getId();
-                         txtnavn.setText(first_name);
+
+
+                         txtnavn.setText(first_name + " " +last_name);
 
                          backgroundWorker.execute("login", id,first_name,last_name);
 
@@ -65,13 +79,9 @@ public class MinSide extends AppCompatActivity {
                     }
                 });
         Bundle parameters = new Bundle();
-        parameters.putString("fields", "id,first_name,last_name");
+        parameters.putString("fields", "id,first_name,last_name,picture");
         request.setParameters(parameters);
-
-
         request.executeAsync();
-
-
 
 
     }
