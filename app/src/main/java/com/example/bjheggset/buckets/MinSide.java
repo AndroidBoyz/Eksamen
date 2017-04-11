e6485f8d0c93f81b190f9fe1c0a520b90603ff15
Bjørnar Heggset Nes <bjornar.hn@hotmail.com>
2017-04-11 19:44:47 +0200

    Lagt til metode for å spørre om tilganger ved Runtime. Åpner Kamera når man trykker på profilbilde på minside.

--- app/src/main/java/com/example/bjheggset/buckets/MinSide.java
+++ app/src/main/java/com/example/bjheggset/buckets/MinSide.java
@@ -1,11 +1,15 @@
 package com.example.bjheggset.buckets;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.hardware.Camera;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,9 +20,12 @@ import com.facebook.AccessToken;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.Profile;
+import com.facebook.common.util.ExceptionWithNoStacktrace;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.facebook.login.widget.ProfilePictureView;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -29,11 +36,11 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import static android.media.MediaRecorder.VideoSource.CAMERA;
+
 public class MinSide extends AppCompatActivity {
     Intent i;
     Context $me = this;
-
-
 
 
     @Override
@@ -47,8 +54,6 @@ public class MinSide extends AppCompatActivity {
         final String userID = getIntent().getExtras().getString("userID");
 
 
-
-
         GraphRequest request = GraphRequest.newMeRequest(
                 AccessToken.getCurrentAccessToken(),
                 new GraphRequest.GraphJSONObjectCallback() {
@@ -60,29 +65,26 @@ public class MinSide extends AppCompatActivity {
                         //Henter facebookbilde via API med userID og streamer til fresco biblioteket
                         Profile profile = Profile.getCurrentProfile();
                         BackgroundWorker backgroundWorker = new BackgroundWorker($me);
-                        TextView txtnavn = (TextView)findViewById(R.id.Navn);
+                        TextView txtnavn = (TextView) findViewById(R.id.Navn);
                         Uri imageUri = Uri.parse("http://graph.facebook.com/" + profile.getId() + "/picture?type=large");
                         SimpleDraweeView draweeView = (SimpleDraweeView) findViewById(R.id.sdvImage);
                         draweeView.setImageURI(imageUri);
 
 
-
-                     try {
-                         String first_name = object.getString("first_name");
-                         String last_name = object.getString("last_name");
-                         String id = Profile.getCurrentProfile().getId();
-
-
-                         txtnavn.setText(first_name + " " +last_name);
-
-                         backgroundWorker.execute("login", id,first_name,last_name);
+                        try {
+                            String first_name = object.getString("first_name");
+                            String last_name = object.getString("last_name");
+                            String id = Profile.getCurrentProfile().getId();
 
 
-                     }
-                     catch(JSONException e)
-                     {
-                         e.getMessage();
-                     }
+                            txtnavn.setText(first_name + " " + last_name);
+
+                            backgroundWorker.execute("login", id, first_name, last_name);
+
+
+                        } catch (JSONException e) {
+                            e.getMessage();
+                        }
                     }
                 });
         Bundle parameters = new Bundle();
@@ -93,14 +95,49 @@ public class MinSide extends AppCompatActivity {
 
     }
 
-    public void ItemActivity(View view){
+    public void ItemActivity(View view) {
         Intent i = new Intent(this, NewItem.class);
         startActivity(i);
     }
 
-    public void getBuckets(View view){
+    public void getBuckets(View view) {
         String userID = Profile.getCurrentProfile().getId();
         BackgroundWorker backgroundWorker = new BackgroundWorker(this);
         backgroundWorker.execute("getLists", userID);
     }
-}
+
+    //Metode for å spørre om tilgang til kamera. Om kamera ikke finnes på enheten får bruker melding
+    public void camera(View view) {
+
+        PackageManager pm = this.getPackageManager();
+
+        int PERMISSION_ALL = 1;
+        String[] PERMISSIONS = {Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CONTACTS, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_SMS, Manifest.permission.CAMERA};
+
+        if (!hasPermissions(this, PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
+            if (pm.hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+                Intent i = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivity(i);
+            }
+            Toast t = Toast.makeText(this, "No camera found on this device", Toast.LENGTH_SHORT);
+            t.show();
+
+    }
+
+//metode for å spørre etter tilganger(Kamera,minnekort,kontakter etc) ved runtime
+
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    }
+
